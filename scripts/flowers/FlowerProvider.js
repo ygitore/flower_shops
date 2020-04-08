1022abb06b8ff2866520a88d3aedcc0478899021
yitbarekgitore <yitbareky@gmail.com>
2020-04-08 14:15:26 -0500

    added component three components. one that fetchs flowers data from api. added another component that takes object and converts to html representation of object and the other component gets all flowers and iterates over each flowers renders on the dom

--- scripts/flowers/FlowerProvider.js
+++ scripts/flowers/FlowerProvider.js
@@ -5,3 +5,4 @@ export const useFlowers = () => flowers.slice()
 export const getFlowers = () => fetch("http://localhost:8088/flowers")
     .then(res => res.json())
     .then(parsedFlowers => flowers = parsedFlowers)
+
